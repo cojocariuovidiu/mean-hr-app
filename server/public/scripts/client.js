ef0989e5fdf91fde0576dedebef02b276d33d4fd
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-25 16:55:27 -0600

    added client.js config

---
+++ server/public/scripts/client.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial']);
+
+// app configuration
+app.config(function($routeProvider) {
+    console.log('config loaded');
+    // define our client side routes
+    $routeProvider
+        .when('/addEmployee', {
+            templateUrl: '/views/addEmployee.view.html',
+            controller: 'AddEmployeeController as vm'
+        })
+        .when('/trackEmployee', {
+            templateUrl: '/views/trackEmployee.view.html',
+            controller: 'TrackEmployeeController as vm'
+        })
+        .when('/reports', {
+            templateUrl: '/views/reports.view.html',
+            controller: 'ReportsController as vm'
+        })
+        .otherwise(
+            { redirectTo: '/reports' }
+        );
+    
+});
