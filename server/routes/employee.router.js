9b9fb36a63bb746bfef09f0c6cf7fc1bf5b134eb
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-26 11:30:04 -0600

    POST and GET for the employee. Service and route updated

--- server/routes/employee.router.js
+++ server/routes/employee.router.js
@@ -7,7 +7,38 @@ let employeeSchema = employeeSchemaModule;
     
 
 
+// GET route
+router.get('/', (req, res) => {
+    Employee.find({}, (error, foundEmployees) => {
+        if(error) {
+            console.log('error on find:', error);
+            res.sendStatus(500);
+        } else {
+            console.log('found employee documents', foundEmployees);
+            res.send(foundEmployees);
+        }
+    })
+});
 
+
+// POST route   
+router.post('/', (req, res) => {
+    console.log('req.body- data to save:', req.body);
+    
+    // create an instance of our model, as an a mongoose object
+    let employeeToAdd = new Employee(req.body);
+
+    // create a new document in our database and collection
+    employeeToAdd.save((error, savedEmployee) => {
+        if(error) {
+            console.log('error on save:', error);
+            res.sendStatus(500);
+        } else {
+            console.log('new employee document', savedEmployee);
+            res.sendStatus(201);
+        }
+    });
+});
 
 
 
