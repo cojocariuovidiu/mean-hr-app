fa2589ddaecc5a44382b32267a4ac7e341f21efe
Ian Robertson <Ianmr08@gmail.com>
2018-01-26 11:33:58 -0600

    added delete and update requests on router and service

--- server/public/scripts/services/employee.service.js
+++ server/public/scripts/services/employee.service.js
@@ -2,4 +2,28 @@ myApp.service('EmployeeService', ['$http', function($http){
     console.log('EmployeeService loaded');
     
     const self = this;
+
+    
+    // updating employee by Id
+    self.updateEmployee = function (employee) {
+        $http.put(`/employee/${employeeId}`, employee)
+        .then(function (response) {
+            console.log('successful updateEmployee: ', response);
+            self.getEmployee();  
+        })
+        .catch(function (response) {
+            console.log('error on updatedEmployee :', response);
+        })
+    }
+    // Deleting employee by Id
+    self.deleteEmployee = function (employeeId) {
+        $http.put(`/employee/${employeeId}`)
+        .then(function (response) {
+            console.log('successful deleteEmployee: ', response);
+            self.getEmployee();  
+        })
+        .catch(function (response) {
+            console.log('error on deleteEmployee :', response);
+        })
+    }
 }]);
--- server/routes/employee.router.js
+++ server/routes/employee.router.js
@@ -3,10 +3,45 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const employeeSchemaModule = require('../modules/schema.module');
 
+
+
 let employeeSchema = employeeSchemaModule;
+let Employee = mongoose.model('Employee', employeeSchema);
     
+// Deleting Employee by ID from table
+router.delete('/:id', (req, res) => {
+    let employeeId = req.params.id;
+    Employee.findByIdAndRemove(
+        {'_id': employeeId},
+        (error, removedDocument) => {
+            if(error) {
+                console.log('error on delete', error);
+                res.sendStatus(500);
+            } else {
+                console.log('document has been removed', removedDocument);
+                res.sendStatus(201);
+            }
+        });
+});
 
-
+// update Employee Info
+router.put('/:id', (req, res) => {
+    let employeeId = req.params.id;
+    let employeeToUpdate = req.body;
+    Employee.findByIdAndUpdate(
+            {'_id': employeeId},
+            {$set: employeeToUpdate},
+            (error, updatedEmployee) => {
+                if(error){
+                    console.log('error on employee update', error);
+                    res.sendStatus(500);
+                } else {
+                    console.log('updated Employee', updatedEmployee);
+                    res.sendStatus(200);
+                }
+            }
+    )
+})
 
 
 
